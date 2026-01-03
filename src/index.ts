f8462bcfb288ad5bd770be668c5d065584fe3355
praveenrusiru <praveenrusiru752@gmail.com>
2026-01-03 19:11:33 +0530

    final

--- src/index.ts
+++ src/index.ts
@@ -31,9 +31,9 @@ const app = express()
  
 
 
-const allowedOrigins = ['http://localhost:8080']; // add your allowed origins here
+// const allowedOrigins = ['https://flower-boquet-frontend.vercel.app/']; // add your allowed origins here
 app.use(cors({
-  origin: allowedOrigins,
+  origin: ['https://flower-boquet-frontend.vercel.app',"http://localhost:8080"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
